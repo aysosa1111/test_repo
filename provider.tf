11190dceb6ab646ba03a380f3dce50b787c5ec06
aysosa1111 <aysosa1111@gmail.com>
2022-12-17 12:28:02 -0500

    test4

--- provider.tf
+++ provider.tf
@@ -1,21 +1,22 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.36.0"
+    required_providers {
+      azurerm ={
+        source +"hashicorp/azurerm"
+        version = "3.36.0"
+      }
     }
-  }
 
-  backend "remote" {
-    organization = "ays11"
+    backend "remote" {
+        organization = "ays11"
 
-    workspace {
-        name = "test_repo"
+        workspaces {
+          name = "test_repo"
+        }
     }
- }
-provider "azurerm" {
-  features{
-    }
-    # Configuration options
 }
+
+provider "azurerm" {
+  features {
+    
+  }
 }
